9ac4f9ad12ad6fca82cbb905365d818e64672296
Tanvi <tanvibirla7@gmail.com>
2023-09-18 23:03:11 +0530

    Login functions

--- BookForYou/books/views.py
+++ BookForYou/books/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from django.contrib.auth import authenticate
+#Password for test user is tanvi@123
 # Create your views here.
 def index(request):
     # context = {
     #     'variable' : "This is sent"
     # }
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render(request,'index.html')
 
 def about(request):
@@ -12,6 +15,22 @@ def about(request):
 
 def contact(request):
     return render(request,'contact.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            return redirect("/")
+        else:
+            return render(request,'login.html')
+    
+    return render(request,'login.html')
+
+def logout(request):
+    return render(request,'index.html')
+
 
 
 
